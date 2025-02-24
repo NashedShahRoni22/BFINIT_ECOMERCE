4edd74a3e3d0eec90de854d2136d929be69c8278
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2025-02-24 17:58:00 +0600

    feat: create store form design improved

--- src/pages/admin/CreateStore/CreateStore.jsx
+++ src/pages/admin/CreateStore/CreateStore.jsx
@@ -1,134 +1,252 @@
+import { useState } from "react";
+import { LuImagePlus } from "react-icons/lu";
+const themes = [
+  {
+    id: 1,
+    name: "black theme",
+    primary: "#000000",
+    accent: "#ff6900",
+    text: "#fff",
+  },
+  {
+    id: 2,
+    name: "blue theme",
+    primary: "#1e96fc",
+    accent: "#FF8C42",
+    text: "#fff",
+  },
+  {
+    id: 3,
+    name: "green theme",
+    primary: "#2f855a",
+    accent: "#84cc16",
+    text: "#fff",
+  },
+  {
+    id: 4,
+    name: "cream theme",
+    primary: "#faf3e0",
+    accent: "#c084fc",
+    text: "#000",
+  },
+];
+
 export default function CreateStore() {
+  const [selectedThemes, setSelectedThemes] = useState("");
+
   return (
-    <section className="px-5">
-      <h1 className="mt-5 text-center text-xl font-semibold capitalize">
-        Please provide your store information
+    <section className="p-5">
+      <h1 className="text-center text-xl font-semibold capitalize">
+        Create new store
       </h1>
 
-      <form>
-        {/* name */}
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            required
-            className="mt-2 w-full rounded border px-4 py-1 outline-none"
-          />
-        </div>
-
+      <form className="mt-8 space-y-5">
         {/* logo */}
-        <div>
-          <label htmlFor="logo">Logo:</label>
-          <input
-            type="file"
-            name="logo"
-            id="logo"
-            className="mt-2 w-full rounded border px-4 py-1 outline-none"
-          />
+        <div className="grid gap-8 md:grid-cols-2">
+          <div>
+            <label htmlFor="logo" className="text-sm font-medium">
+              Logo:
+            </label>
+            <input type="file" name="logo" id="logo" className="hidden" />
+            <div className="mt-2 w-full rounded border border-dashed border-neutral-300 bg-neutral-50 px-3 py-6 text-center">
+              <LuImagePlus className="mx-auto text-2xl text-neutral-500" />
+              <div className="mt-1.5 text-sm text-neutral-500">
+                <label
+                  htmlFor="logo"
+                  className="text-primary cursor-pointer underline"
+                >
+                  Click to upload
+                </label>{" "}
+                & <br />
+                select your store logo
+              </div>
+            </div>
+          </div>
+
+          {/* favicon */}
+          <div>
+            <label htmlFor="logo" className="text-sm font-medium">
+              Favicon:
+            </label>
+            <input type="file" name="favicon" id="favicon" className="hidden" />
+            <div className="mt-2 w-full rounded border border-dashed border-neutral-300 bg-neutral-50 px-3 py-6 text-center">
+              <LuImagePlus className="mx-auto text-2xl text-neutral-500" />
+              <div className="mt-1.5 text-sm text-neutral-500">
+                <label
+                  htmlFor="logo"
+                  className="text-primary cursor-pointer underline"
+                >
+                  Click to upload
+                </label>{" "}
+                & <br />
+                select your website favicon
+              </div>
+            </div>
+          </div>
         </div>
 
-        {/* favicon */}
-        <div>
-          <label htmlFor="logo">Favicon:</label>
-          <input
-            type="file"
-            name="favicon"
-            id="favicon"
-            className="mt-2 w-full rounded border px-4 py-1 outline-none"
-          />
-        </div>
-
-        {/* theme */}
-        <p>Choose Theme</p>
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4">
+        {/* store name & contact info */}
+        <div className="grid grid-cols-2 gap-x-8 gap-y-4">
           <div>
-            <input type="checkbox" name="theme1" id="theme1" />
-            <label htmlFor="theme1">Theme1</label>
+            <label htmlFor="name" className="text-sm font-medium">
+              Store Name:
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
+            />
           </div>
           <div>
-            <input type="checkbox" name="theme1" id="theme1" />
-            <label htmlFor="theme1">Theme1</label>
+            <label htmlFor="email" className="text-sm font-medium">
+              Email:
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              required
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
+            />
           </div>
           <div>
-            <input type="checkbox" name="theme1" id="theme1" />
-            <label htmlFor="theme1">Theme1</label>
+            <label htmlFor="mobile" className="text-sm font-medium">
+              Mobile No:
+            </label>
+            <input
+              type="text"
+              name="mobile"
+              id="mobile"
+              required
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
+            />
           </div>
           <div>
-            <input type="checkbox" name="theme1" id="theme1" />
-            <label htmlFor="theme1">Theme1</label>
+            <label htmlFor="telephone" className="text-sm font-medium">
+              Telephone:
+            </label>
+            <input
+              type="text"
+              name="telephone"
+              id="telephone"
+              required
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
+            />
+          </div>
+          <div className="col-span-full">
+            <label htmlFor="address" className="text-sm font-medium">
+              Address:
+            </label>
+            <textarea
+              rows={3}
+              name="address"
+              id="address"
+              required
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
+            />
           </div>
         </div>
 
         {/* social info */}
-        <p>Social Links:</p>
-        <div className="grid grid-cols-2 gap-4">
+        <div className="grid grid-cols-2 gap-x-8 gap-y-4">
           <div>
-            <label htmlFor="name">Facebook:</label>
+            <label htmlFor="facebook" className="text-sm font-medium">
+              Facebook:
+            </label>
             <input
-              type="text"
-              name="name"
-              id="name"
+              type="url"
+              name="facebook"
+              id="facebook"
+              placeholder="https://facebook.com/john.doe"
               required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
             />
           </div>
           <div>
-            <label htmlFor="name">Twitter:</label>
+            <label htmlFor="twitter" className="text-sm font-medium">
+              Twitter/X:
+            </label>
             <input
-              type="text"
-              name="name"
-              id="name"
+              type="url"
+              name="twitter"
+              id="twitter"
+              placeholder="https://x.com/john.doe"
               required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
             />
           </div>
           <div>
-            <label htmlFor="name">Instagram:</label>
+            <label htmlFor="instagram" className="text-sm font-medium">
+              Instagram:
+            </label>
             <input
-              type="text"
-              name="name"
-              id="name"
+              type="url"
+              name="instagram"
+              id="instagram"
+              placeholder="https://www.instagram.com/john.doe"
               required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
             />
           </div>
           <div>
-            <label htmlFor="name">Youtube:</label>
+            <label htmlFor="youtube" className="text-sm font-medium">
+              Youtube:
+            </label>
             <input
-              type="text"
-              name="name"
-              id="name"
+              type="url"
+              name="youtube"
+              id="youtube"
+              placeholder="https://www.youtube.com/@john.doe"
               required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
+              className="mt-1.5 w-full rounded border border-neutral-200 bg-neutral-50 px-4 py-1 outline-none focus:border-neutral-400"
             />
           </div>
         </div>
 
-        {/* contact info */}
-        <p>Contact Info:</p>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
-            <label htmlFor="name">Mobile No:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
-            />
+        {/* theme */}
+        <div>
+          <p className="text-sm font-medium">Choose Theme:</p>
+          <div className="mt-2 grid grid-cols-2 gap-8 md:grid-cols-4">
+            {themes.map((theme) => (
+              <div
+                key={theme.id}
+                onClick={() => setSelectedThemes(theme.id)}
+                className={`cursor-pointer rounded border bg-neutral-50 p-3.5 ${selectedThemes === theme.id ? "border-primary" : "border-neutral-200"}`}
+              >
+                <div className="flex justify-center gap-1">
+                  <div
+                    style={{ backgroundColor: theme.primary }}
+                    className="size-8 rounded-full border border-neutral-200"
+                  ></div>
+                  <div
+                    style={{ backgroundColor: theme.accent }}
+                    className="size-8 rounded-full border border-neutral-200"
+                  ></div>
+                  <div
+                    style={{ backgroundColor: theme.text }}
+                    className="size-8 rounded-full border border-neutral-200"
+                  ></div>
+                </div>
+                <p
+                  className={`mt-2.5 text-center text-sm font-semibold capitalize ${selectedThemes === theme.id ? "text-neutral-800" : "text-neutral-500"}`}
+                >
+                  {theme.name}
+                </p>
+              </div>
+            ))}
           </div>
-          <div>
-            <label htmlFor="name">Telephone:</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              required
-              className="mt-2 w-full rounded border px-4 py-1 outline-none"
-            />
-          </div>
+        </div>
+
+        {/* submit button */}
+        <div className="mt-12 mb-5 text-center">
+          <button
+            type="submit"
+            className="bg-primary/90 hover:bg-primary cursor-pointer rounded px-4 py-1 text-white transition duration-200 ease-in-out"
+          >
+            Create New Store
+          </button>
         </div>
       </form>
     </section>
